17e1e89707377d90173ee6c78e3a4c2771c60884
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2024-12-23 16:54:01 -0500

    fix: properly set global ignores and overrides
    this now uses the `tseslint.config` utility correctly, providing automatic IDE typings and merges

--- eslint.config.js
+++ eslint.config.js
@@ -1,72 +1,72 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-const tslinter = tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
-
-/** @type { import('eslint').Linter.Config } */
-const configs = {
-	languageOptions: {
-		sourceType: 'module',
-		ecmaVersion: 6,
+export default tseslint.config(
+	{
+		ignores: ['@types', 'build', 'dist', 'gulpfile.ts', 'stylelint.config.js', '_generated', '@generated'],
 	},
-	ignores: ['@types', 'build', 'dist', 'gulpfile.ts', 'stylelint.config.js', '_generated', '@generated'],
-	rules: {
-		camelcase: 'error',
-		quotes: ['error', 'single', { allowTemplateLiterals: true }],
-		semi: 'off',
-		indent: 'off',
-		// indent: ['error', 'tab', { VariableDeclarator: { var: 2, let: 2, const: 3 }, SwitchCase: 1 }],
-		'no-tabs': 'off',
-		// 'no-tabs': ['error', { allowIndentationTabs: true }],
-		'no-trailing-spaces': ['error', { skipBlankLines: true }],
-		'array-bracket-newline': ['error', { multiline: true }],
-		'implicit-arrow-linebreak': ['error', 'beside'],
-		'no-whitespace-before-property': 'error',
-		'no-multi-spaces': 'error',
-		'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-		'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-		'no-confusing-arrow': 'error',
-		'padded-blocks': ['error', 'never', { allowSingleLineBlocks: true }],
-		'eol-last': ['error', 'always'],
-		'no-lonely-if': 'error',
-		'no-underscore-dangle': 'error',
-		'semi-style': ['error', 'last'],
-		'padding-line-between-statements': [
-			'error',
-			{ blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-			{ blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-			{ blankLine: 'always', prev: 'directive', next: '*' },
-			{ blankLine: 'any', prev: 'directive', next: 'directive' },
-		],
-		'no-unused-vars': 'off',
-		'keyword-spacing': ['error', { before: true, overrides: { this: { before: false } } }],
-		'space-before-blocks': 'error',
-		'arrow-spacing': 'error',
-		'space-infix-ops': ['error', { int32Hint: false }],
-		'no-empty-function': 'off',
-		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-empty-function': ['error', { allow: ['private-constructors'] }],
-		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-magic-numbers': [
-			'warn',
-			{
-				ignoreArrayIndexes: true,
-				ignoreTypeIndexes: true,
-				ignoreEnums: true,
-				ignoreReadonlyClassProperties: true,
-				ignore: [-1, 0, 1, 1000, 60, 24],
-			},
-		],
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		// "@typescript-eslint/no-explicit-any": "off",
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		languageOptions: {
+			sourceType: 'module',
+			ecmaVersion: 6,
+		},
+		rules: {
+			camelcase: 'error',
+			quotes: ['error', 'single', { allowTemplateLiterals: true }],
+			semi: 'off',
+			indent: 'off',
+			// indent: ['error', 'tab', { VariableDeclarator: { var: 2, let: 2, const: 3 }, SwitchCase: 1 }],
+			'no-tabs': 'off',
+			// 'no-tabs': ['error', { allowIndentationTabs: true }],
+			'no-trailing-spaces': ['error', { skipBlankLines: true }],
+			'array-bracket-newline': ['error', { multiline: true }],
+			'implicit-arrow-linebreak': ['error', 'beside'],
+			'no-whitespace-before-property': 'error',
+			'no-multi-spaces': 'error',
+			'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+			'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+			'no-confusing-arrow': 'error',
+			'padded-blocks': ['error', 'never', { allowSingleLineBlocks: true }],
+			'eol-last': ['error', 'always'],
+			'no-lonely-if': 'error',
+			'no-underscore-dangle': 'error',
+			'semi-style': ['error', 'last'],
+			'padding-line-between-statements': [
+				'error',
+				{ blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+				{ blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+				{ blankLine: 'always', prev: 'directive', next: '*' },
+				{ blankLine: 'any', prev: 'directive', next: 'directive' },
+			],
+			'no-unused-vars': 'off',
+			'keyword-spacing': ['error', { before: true, overrides: { this: { before: false } } }],
+			'space-before-blocks': 'error',
+			'arrow-spacing': 'error',
+			'space-infix-ops': ['error', { int32Hint: false }],
+			'no-empty-function': 'off',
+			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+			'@typescript-eslint/interface-name-prefix': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-empty-function': ['error', { allow: ['private-constructors'] }],
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/no-magic-numbers': [
+				'warn',
+				{
+					ignoreArrayIndexes: true,
+					ignoreTypeIndexes: true,
+					ignoreEnums: true,
+					ignoreReadonlyClassProperties: true,
+					ignore: [-1, 0, 1, 1000, 60, 24],
+				},
+			],
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			// "@typescript-eslint/no-explicit-any": "off",
+		},
 	},
-};
-
-/** @type { import('eslint').Linter.Config[] } */
-const overrides = [
+	// overrides
 	{
 		files: ['**/*.{e2e-spec,spec,test}.ts', '**/{tests,prisma}/**/*.ts'],
 		rules: {
@@ -88,6 +88,4 @@ const overrides = [
 			camelcase: 'off',
 		},
 	},
-];
-
-export default [...tslinter, configs, ...overrides];
+);
